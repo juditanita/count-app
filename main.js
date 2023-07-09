9ef800b1bb3fb37f6bd19ca45ef35fa8f797739f
juditanita <101408621+juditanita@users.noreply.github.com>
2023-07-09 20:17:59 +0200

    modify responsive

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ incrementBtn.addEventListener("click", function () {
 });
 
 function Save() {
-  let countStr = count + " - ";
+  let countStr = " " + count + " - ";
   saveEl.textContent += countStr;
   countEl.textContent = 0;
   count = 0;
