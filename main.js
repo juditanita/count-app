0d559810956ae71190122e6933c53d1ed7d5e008
juditanita <101408621+juditanita@users.noreply.github.com>
2023-07-09 16:38:16 +0200

    add files

---
+++ main.js
@@ -0,0 +1,20 @@
+const countEl = document.getElementById("count-el");
+const incrementBtn = document.getElementById("increment-btn");
+const saveBtn = document.getElementById("save-btn");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+incrementBtn.addEventListener("click", function () {
+  count += 1;
+  countEl.textContent = count;
+
+  console.log(count);
+});
+
+function Save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+  console.log(countStr);
+}
